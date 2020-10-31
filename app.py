55cbf141e3d20d0a1ca94e1a98f29cfb37919a30
Michelle Nguyen <65826269+nmeeshell@users.noreply.github.com>
2020-10-31 12:47:29 -0500

    Update app.py
    Updated

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
 from flask import Flask, jsonify
+import numpy as np
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.pool import StaticPool
 
 app = Flask(__name__)
 
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={"check_same_thread": False}, poolclass=StaticPool, echo=True)
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
 
 @app.route("/")
-def home():
-    return "Hello, world!"
-
+def welcome():
+     """List all available api routes."""
+    return ()
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
 
 @app.route("/api/v1.0/precipitation")
-def date():
-    return ""
+def precipitation():
+    query_year = dt.date(2017,8,23) - dt.timedelta(days = 365)
+    prcp_data = session.query(Measurement.date, Measurement.prcp).\
+                filter(Measurement.date >= query_year).\
+                order_by(Measurement.date).all()
+    prcp_data_list = dict(prcp_data)
+    return jsonify(prcp_data_list)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
-    return ""
+        stations = session.query(func.count(Station.station))
+        station_count = stations[0]
+        return jsonify(station_count)
 
 
 @app.route("/api/v1.0/tobs")
@@ -28,6 +55,23 @@ filter(Measure.date >= query_year).all()
         }
     )
 
+
+@app.route("/api/v1.0/<start>")
+def start_day(start):
+        start_day = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).\
+                group_by(Measurement.date).all(
+        start_day_list = list(start_day)
+        return jsonify(start_day_list)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_day(start, end):
+        start_end_day = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).\
+                filter(Measurement.date <= end).\
+                group_by(Measurement.date).all(
+        start_end_day_list = list(start_end_day)
+        return jsonify(start_end_day_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
